4bccd07f40219690de87d6811aaf777a2c59b7d2
ludomare <108358050+ludovicmarecaux@users.noreply.github.com>
2023-09-20 23:20:04 +0200

    model trainer & evaluate model from utils

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,7 +10,7 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
-
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -52,8 +52,12 @@ if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
 
-    datatransf=DataTransformation()
-    datatransf.initiate_data_transformation(train_data,test_data)
+    data_transformation=DataTransformation()
+    logging.info("data transformation sollicité")
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
 
 
